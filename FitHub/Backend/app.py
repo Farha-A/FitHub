2f5da231088f793ca13895e64632865f2841dc20
ArwaI1 <132764476+ArwaI1@users.noreply.github.com>
2024-11-29 15:36:12 +0200

    initial posts fn

--- FitHub/Backend/app.py
+++ FitHub/Backend/app.py
@@ -119,7 +119,60 @@ def coachSignUp():
         conn.close()
         return redirect(url_for('role_choice'))
     return render_template("coachSignUp.html")
+    
 
+@app.route('/', methods=['GET', 'POST'])
+def posts_screen():
+    if 'Email' in session:
+        Email = session['Email']
+        conn = get_db_connection()
+
+        # fetch user data
+        user = conn.execute('SELECT * FROM User WHERE Email = ?', (Email,)).fetchone()
+
+# share post 
+        if request.method == 'POST' and 'post_content' in request.form:
+            post_content = request.form['post_content']
+            post_media = request.form['media'] if 'media' in request.form else None  
+            conn.execute(
+                'INSERT INTO Posts (User_ID, Content, Time_Stamp, Tags, Media) VALUES (?, ?, datetime("now"), ?, ?)',
+                (user['User_ID'], post_content, user['Interests'], post_media)
+            )
+            conn.commit()
+# posts
+        user_interests = user['Interests'].split(',')
+        # map interest with their IDs
+        placeholders = ', '.join(['?'] * len(user_interests))
+        interest_ids = conn.execute('SELECT Interest_ID FROM Interest WHERE Name IN (' + placeholders + ')',
+        user_interests).fetchall()
+
+        # make alist of interests
+        interest_ids = [str(row['Interest_ID']) for row in interest_ids] 
+
+        placeholders = ', '.join(['?'] * len(interest_ids))
+        posts_by_interest = conn.execute(
+            '''SELECT * FROM Posts WHERE EXISTS (
+                SELECT 1 FROM Interest 
+                WHERE Interest.Interest_ID IN (' + placeholders_for_interest_ids + ') 
+                  AND Posts.tags LIKE '%' || Interest.Interest_ID || '%') ORDER BY created_at DESC''', 
+                  interest_ids).fetchall()
+        
+        remaining_posts = conn.execute(
+            '''SELECT * FROM Posts WHERE id NOT IN (
+                SELECT id FROM Posts 
+                WHERE EXISTS (
+                    SELECT 1 FROM Interest 
+                    WHERE Interest.Interest_ID IN (' + placeholders_for_interest_ids + ') 
+                      AND Posts.tags LIKE '%' || Interest.Interest_ID || '%')) ORDER BY created_at DESC''',
+                    interest_ids).fetchall()
+            
+        all_posts = posts_by_interest + remaining_posts
+
+
+        conn.close()
+
+        return render_template("posts_screen.html", user=user, posts=all_posts)
+    return redirect(url_for('login')) 
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
